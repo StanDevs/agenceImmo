2bb0f1ba62482f3225b90a8685f43fa808785f0a
KillGuil <selin.ispir@etu.univ-lyon1.fr>
2021-03-22 08:38:18 +0100

    ajout home.vue

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="containerList">
+      <div class="item" v-for="achat in achats" :key="achat">
+        <h2>{{achat.titre}}</h2>
+        <p>{{achat.description}}</p>
+        <strong>
+          {{Intl.NumberFormat(
+          'fr-FR', { style: 'currency', currency: 'EUR', maximumFractionDigits: '0' }
+          ).format(achat.prix)}}
+        </strong>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
   },
+  data: () => ({
+    achats: [
+      { titre: 'Maison Bourg', description: 'ceci est la description', prix: 180000 },
+      { titre: 'Maison Bourg2', description: 'ceci est la deuxieme description', prix: 200000 },
+      { titre: 'Maison Bourg2', description: 'ceci est la deuxieme description', prix: 200000 },
+      { titre: 'Maison Bourg2', description: 'ceci est la deuxieme description', prix: 200000 },
+      { titre: 'Maison Bourg2', description: 'ceci est la deuxieme description', prix: 200000 },
+      { titre: 'Maison Bourg2', description: 'ceci est la deuxieme description', prix: 110000 },
+      { titre: 'Maison Bourg2', description: 'ceci est la deuxieme description', prix: 200000 },
+      { titre: 'Maison Bourg2', description: 'ceci est la deuxieme description', prix: 200000 },
+    ],
+  }),
 };
 </script>
+
+<style>
+  .containerList {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 18px;
+  }
+
+  .item {
+    border: 5px #42b983 solid;
+  }
+</style>
