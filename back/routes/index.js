25f302e9dcf322b914c69adf21bcb5abad869863
KillGuil <selin.ispir@etu.univ-lyon1.fr>
2021-03-22 09:10:00 +0100

    modif routes back

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,8 +1,26 @@
-var express = require('express');
+const express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  //récupère un paramète de route (ici id)
+  const id = req.params.id;
+  
+  //Récupère le body envoyé dans la requête
+  const body =req.body;
+
+  //Récupère la qquery passée par l'url
+  //ex : example.com/ma-route/42?name=Emilien
+  const name =req.query.name
+
+  //réponse
+  res.send({info: 'envoyé'});
+
+  //réponse avec code http custom
+  res.status(403).send({info:'interdit'});
+
+
+  //jsp
   res.render('index', { title: 'Express' });
 });
 
